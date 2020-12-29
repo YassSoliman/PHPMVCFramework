95b6fd124f20813d56642d41a2a63d5cedfb613c
YassSoliman <soliman.yasser@hotmail.com>
2020-12-29 16:37:15 -0500

    Implemented SiteController

---
+++ controllers/SiteController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace app\controllers;
+use app\core\Application;
+
+class SiteController
+{
+	public function home()
+	{
+		$params = [
+			'name' => 'Yasser'
+		];
+		return Application::$app->router->renderView('home', $params);		
+	}
+
+	public function contact()
+	{
+		return Application::$app->router->renderView('contact');		
+	}
+
+	public function handleContact()
+	{
+		return 'Handling submitted data';
+	}
+	
+}
+
+?>
--- public/index.php
+++ public/index.php
@@ -2,15 +2,14 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 use app\core\Application;
+use app\controllers\SiteController;
 
 $app = new Application(dirname(__DIR__));
 
-$app->router->get('/', 'home');
+$app->router->get('/', [new SiteController, 'home']);
 
-$app->router->get('/contact', 'contact');
-$app->router->post('/contact', function(){
-	return 'handling submitted data';
-});
+$app->router->get('/contact', [new SiteController, 'contact']);
+$app->router->post('/contact', [new SiteController, 'handleContact']);
 
 $app->run();
 /*
